0cb4ddf8e6e6817a3f7cb5b83205deebccc5d167
lihuang-zheng <jerryzheng0131@gmail.com>
2022-03-02 10:32:26 -0600

    base structure of spring framework.

---
+++ src/main/java/com/sparkybubble/numbertostring/NumberToStringApplication.java
@@ -0,0 +1,13 @@
+package com.sparkybubble.numbertostring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberToStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberToStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparkybubble/numbertostring/NumberToStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkybubble.numbertostring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberToStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
