1162535039b0715fb83579a7241a7a361a08bb1c
Edgar <edvlicup@gmail.com>
2022-03-04 07:57:15 -0800

    Added Streams to the display() function

--- src/main/java/com/sparkybubble/numbertostring/NumberToStringApplication.java
+++ src/main/java/com/sparkybubble/numbertostring/NumberToStringApplication.java
@@ -20,63 +20,6 @@ public class NumberToStringApplication {
 
 	public static void display() throws IOException {
 		List<String> data = readIntoFile();
-//		int numOfCols = data.size() <= 8 ? 2 : 3;
-//		int numOfRows = numOfCols == 2 ? 4 : 9;
-//		String[][] array = new String[numOfRows][numOfCols];
-//		int indx = 0;
-//		if(numOfCols == 2) {
-//			for (int row = 0; row < numOfRows; row++) {
-//				if (row >= 3) {
-//					array[row][0] = data.get(indx);
-//					indx++;
-//					for (int r2 = 0; r2 < numOfRows; r2++) {
-//						if (indx == data.size()) {
-//							break;
-//						}
-//						array[r2][1] = data.get(indx);
-//						indx++;
-//					}
-//				} else {
-//					array[row][0] = data.get(indx);
-//					indx++;
-//				}
-//			}
-//			System.out.println(data);
-//		}else{
-//			//column 1
-//			for (int r1 = 0; r1 < numOfRows; r1++) {
-//				if (r1 >= 8) {
-//					array[r1][0] = data.get(indx);
-//					indx++;
-//					//column 2
-//					for (int r2 = 0; r2 < numOfRows; r2++) {
-//						if (r2 >= 8) {
-//							array[r2][1] = data.get(indx);
-//							indx++;
-//							//column 3
-//							for (int r3 = 0; r3 < numOfRows; r3++) {
-//								if (indx == data.size()) {
-//									break;
-//								}if(r3 >= 8){
-//									array[r3][2] = data.get(indx);
-//									indx++;
-//								}else {
-//									array[r3][2] = data.get(indx);
-//									indx++;
-//								}
-//							}
-//						}else {
-//							array[r2][1] = data.get(indx);
-//							indx++;
-//						}
-//					}
-//				} else {
-//					array[r1][0] = data.get(indx);
-//					indx++;
-//				}
-//			}
-//			System.out.println(data);
-//		}
 		String[][] array = sortArray(data);
 
 		String[][] filteredArr = Arrays.stream(array)
