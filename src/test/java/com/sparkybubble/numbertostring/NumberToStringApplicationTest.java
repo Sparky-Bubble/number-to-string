ad447d3b186c1cddc7e2bad7987f49b2bb14d893
Devan <devan_mcneal@yahoo.com>
2022-03-05 11:35:19 -0500

    Added comments to readIntoFileTest.

--- src/test/java/com/sparkybubble/numbertostring/NumberToStringApplicationTest.java
+++ src/test/java/com/sparkybubble/numbertostring/NumberToStringApplicationTest.java
@@ -31,17 +31,24 @@ class NumberToStringApplicationTest {
     void testReadIntoFile() throws IOException {
 
         File file = new File("src/main/resources/nums.txt");
+
+        /*Tests whether the file is located and also can be read.*/
         assertTrue(file.exists());
         assertTrue(file.canRead());
 
+        /*Create another file equal to the first*/
         File file2 = file;
         assertTrue(file2.exists());
 
+        /*Test to make sure both refer to same file*/
         assertTrue(file.getAbsoluteFile().equals(file2.getAbsoluteFile()));
 
+        /*Test to see if the readIntoFile method is not empty when called.*/
         assertFalse(readIntoFile().isEmpty());
+        /*Assert that the size of the file is the same*/
         assertTrue(readIntoFile().size() == 7);
 
+        /*Try to add to the file, then call method again to make sure nothing is changed.*/
         readIntoFile().add("eight");
         assertFalse(readIntoFile().size() == 8);
 
